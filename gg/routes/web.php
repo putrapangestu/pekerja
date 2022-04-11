34e106bf9c944598d293fdff55055a2075eccfc7
putrapangestu <putraprp8@gmail.com>
2022-04-11 11:25:08 +0700

    Update web.php

--- gg/routes/web.php
+++ gg/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\GabutController;
+use App\Http\Controllers\CobaController;
+use App\http\Controllers\ContactController;
+use App\Http\Controllers\RekrutController;
 use App\Http\Controllers\GaweController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,6 +30,16 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/', [GaweController::class, 'index'])->name('/');
 Route::get('/listings', [GabutController::class, 'listings'])->name('listings');
 Route::get('/detail-pekerja/{telepon}', [GabutController::class, 'listingsdetails'])->name('detail-pekerja');
+Route::get('/profile/{email}', [ProfileController::class, 'index'])->name('profile');
+Route::get('/edit-profile/{email}', [ProfileController::class, 'edit'])->name('edit-profile');
+Route::post('/edit-profile/{email}', [ProfileController::class, 'update'])->name('edit-profile');
+Route::get('/rekrut/{email}', [RekrutController::class, 'index'])->name('rekrut');
+Route::post('/merekrut/{email}', [RekrutController::class, 'store'])->name('merekrut');
+Route::get('/inbox', [CobaController::class, 'index'])->name('inbox');
+Route::get('inbox-detail/{email}', [CobaController::class, 'detail_index'])->name('inbox-detail');
+Route::get("kontak", [ContactController::class, 'index'])->name('kontak');
+Route::post('chat-kontak', [ContactController::class, 'contact'])->name('chat-kontak');
+Route::get('tentang-kami', [GaweController::class, 'about'])->name('tentang-kami');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
